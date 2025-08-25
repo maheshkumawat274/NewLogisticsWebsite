e6432f0202020ee07a6fdb85e75c8dc25f960320
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-08-25 18:08:16 +0530

    done

--- src/components/home/Operations.tsx
+++ src/components/home/Operations.tsx
@@ -30,7 +30,7 @@ const Operations = () => {
   return (
     <section className="py-4 md:py-10">
       <div className="container mx-auto px-4 md:px-10 text-center">
-        <h2 className="text-3xl font-bold text-gray-800 mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12">
           Our Services
         </h2>
 
